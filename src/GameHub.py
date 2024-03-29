49603f593445fa247cd8572b5a086074a563f685
myschoolac <142546434+myschoolac@users.noreply.github.com>
2024-03-29 10:53:10 -0600

    Update GameHub.py

--- src/GameHub.py
+++ src/GameHub.py
@@ -1,1 +1,9 @@
-
+# Game Hub class
+class GameHub:
+    def __init__(self):
+        self.games = {
+            "Rock Paper Scissors": RockPaperScissors(),
+            "Coin Flip": CoinFlip(),
+            "Hangman": Hangman("python"),
+            "War": War()
+        }
